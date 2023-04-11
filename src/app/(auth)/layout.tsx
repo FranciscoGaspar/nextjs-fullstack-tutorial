16612410b4e18dfb9bb688a0aaca22debc68d646
Francisco Gaspar <f.gaspar@kigroup.de>
2023-04-11 12:42:49 +0100

    feat(project-app): add authform to register and signin

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ export default function AuthRootLayout({children}) {
         <html lang="en">
             <head />
             <body className="h-screen w-screen rainbow-mesh p-6">
-                <GlassPane className="w-full h-full flex items-center">
+                <GlassPane className="w-full h-full flex items-center justify-center">
                     {children}
                 </GlassPane>
             </body>
